6bdef9822bd491906e6f4e366ccf5ac81beeaacf
kennedy babu <kennedynjogu8@gmail.com>
2021-08-01 10:09:05 +0300

    add font stylings

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
 let form = document.getElementById("form")
 
+//business logic
 function Pizza(type, size, crust, topping, pieces) {
     this.type = type,
     this.size = size,
     this.crust = crust,
-    this.topping = topping,
+    this.topping = topping
     this.pieces = pieces
 }
 
+Pizza.prototype.getTotal = function(pizzaType, pizzaSize){
+    if(pizzaType == "pepporoni"){
+        return pizzaCost = pepporoniPizza[pizzaSize]
+    }
+}
 
+//user interface logic
 $(document).ready(function(){
 
     $("#form").submit(function(event){
         event.preventDefault()
 
+        let pepporoniPizza = {
+            "large" : 1500,
+            "medium" : 1200,
+            "small" : 1000
+        }
+
+        let pizzaCost = 0    
+
+
+        //get user input
         let pizzaType = $("select#type").val()
         let pizzaSize = $("select#size").val()
         let pizzaCrust = $("select#crust").val()
         let pizzaTopping = $("select#topping").val()
-        let pizzaPieces = $("input#numbers").val()
-
-        
+        let pizzaPieces = $("input#numbers").val()   
         
 
-        // alert("ready to go")
-        console.log(pizzaType, pizzaSize,pizzaCrust,pizzaTopping,pizzaPieces)
+        let orderedPizza = new Pizza(pizzaType, pizzaSize, pizzaCrust, pizzaTopping)
+
+               
+        console.log(orderedPizza.getTotal())
     })
 
 })
