b9655d80f33619af86bf4172be8054232d265eba
kennedy babu <kennedynjogu8@gmail.com>
2021-08-01 07:48:39 +0300

    get user input

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+let form = document.getElementById("form")
+
+function Pizza(type, size, crust, topping, pieces) {
+    this.type = type,
+    this.size = size,
+    this.crust = crust,
+    this.topping = topping,
+    this.pieces = pieces
+}
+
+
 $(document).ready(function(){
-    console.log(70)
+
+    $("#form").submit(function(event){
+        event.preventDefault()
+
+        let pizzaType = $("select#type").val()
+        let pizzaSize = $("select#size").val()
+        let pizzaCrust = $("select#crust").val()
+        let pizzaTopping = $("select#topping").val()
+        let pizzaPieces = $("input#numbers").val()
+
+        
+        
+
+        // alert("ready to go")
+        console.log(pizzaType, pizzaSize,pizzaCrust,pizzaTopping,pizzaPieces)
+    })
 
 })
